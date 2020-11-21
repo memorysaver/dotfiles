8064e508ee978e18e9b6570b49e6608d34f806db
Ming-Cheng Ho <mfa1484@gmail.com>
2020-11-21 07:29:39 +0000

    firx script error

--- install.sh
+++ install.sh
@@ -4,19 +4,14 @@ if [ ! -d "$HOME/.dotfiles" ]; then
   git clone git://github.com/memorysaver/dotfiles.git ~/.dotfiles
 fi
 
-if [ ! -d "$HOME/.dotfiles" ]; then
-  echo "No dotfiles available. Aborting."
-else
-    # generate ssh-keygen
-    if [ ! -f "$HOME/.ssh/id_rsa" ]; then
-        ssh-keygen -t rsa -C "mfa1484@gmail.com"
-    fi
-
-    # symlinks for dotfiles
-    ln -sfv "$HOME/.dotfiles/.spacemacs" ~
-
-    # change origin to push dotfiles
-    cd ~/.dotfiles
-    git remote set-url origin git@github.com:memorysaver/dotfiles.git
-
+# generate ssh-keygen
+if [ ! -f "$HOME/.ssh/id_rsa" ]; then
+    ssh-keygen -t rsa -C "mfa1484@gmail.com"
 fi
+
+# symlinks for dotfiles
+ln -sfv "$HOME/.dotfiles/.spacemacs" ~
+
+# change origin to push dotfiles
+cd ~/.dotfiles
+git remote set-url origin git@github.com:memorysaver/dotfiles.git
